399de1e041337f5fcbb44f980e8d1479214b1a1b
rakinplaban <rakinshahriar54@gmail.com>
2023-04-06 11:52:04 +0600

    token

--- ecommerce/market/urls.py
+++ ecommerce/market/urls.py
@@ -1,11 +1,12 @@
 from . import views
-
 from django.urls import path
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path("allproducts/",views.allproducts),
     path("allclients/",views.allclients),
-    path('addclient/',views.addclient),
+    # path('addclient/',views.addclient),
+    path('categories',views.view_category),
     path('register/', views.register, name='register'),
+    path('login/',views.login,name='login'),
 ]
--- ecommerce/market/views.py
+++ ecommerce/market/views.py
@@ -9,9 +9,25 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .models import *
+from .forms import *
 from .serializers import *
-from rest_framework import generics
+from django.contrib.auth import authenticate, login
+from rest_framework.permissions import AllowAny
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+@authentication_classes([])
+@renderer_classes([BrowsableAPIRenderer, JSONRenderer])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'status':200, 'token':token.key})
+    return Response({'status':401, 'message':'Invalid credentials'})
 
 @api_view(['POST'])
 @renderer_classes([BrowsableAPIRenderer,JSONRenderer])
@@ -43,6 +59,19 @@ def allproducts(request):
         products = Products.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    
+# @api_view(['POST'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
+# def addproduct(request):
+#     if request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = ProductSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+
    
 @api_view(['GET'])
 @renderer_classes([BrowsableAPIRenderer,JSONRenderer])
@@ -53,14 +82,23 @@ def allclients(request):
         return Response(serializer.data)
    
     
-@api_view(['POST'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def addclient(request):
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ClientSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+# @api_view(['POST'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
+# def addclient(request):
+#     if request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = ClientSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+@renderer_classes([BrowsableAPIRenderer,JSONRenderer])
+def view_category(request):
+    if request.method == 'GET':
+        clients = Category.objects.all()
+        serializer = CategoriesSerializer(clients, many=True)
+        return Response(serializer.data)
