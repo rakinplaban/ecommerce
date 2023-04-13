dec64234926e52db32c5f67ac77e7ea004273079
rakinplaban <rakinshahriar54@gmail.com>
2023-04-13 18:04:51 +0600

    product variants api

--- ecommerce/market/urls.py
+++ ecommerce/market/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path("allproducts/",views.allproducts),
     path("allclients/",views.allclients),
     # path('addclient/',views.addclient),
+    path('pricing/<int:id>',views.product_pricing),
     path('categories',views.view_category),
     path('register/', views.register, name='register'),
     path('login/',views.login_view,name='login'),
-    path('logout/',views.logout_view,name='logout')
+    path('logout/',views.logout_view,name='logout'),
+    path('product_thumbnail/<int:id>',views.product_thumbnail),
 ]
